32f49add14374ed2c2f09aeac83ffe88e8adf88c
wuzht <770333760@qq.com>
2018-06-26 00:34:30 +0800

    hw15

--- hw15/part/Classes/Utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include "json\document.h"
-
-std::string serializeRapidjsonDocument(const rapidjson::Document &doc);
-// In C++11, it's ok to use std::to_string
-std::string intToStr(int i);
